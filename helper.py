c6cabdc450219476391ed37c928822fbbd352a41
casey <maxcaseylin@gmail.com>
2019-07-03 23:36:00 -0700

    stuff

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 #get card data
 import json
+import random
 data = open('cards.json', 'r') 
 carddata = json.load(data)
 
@@ -30,6 +31,27 @@ class Player:
         self.deck = []
         self.discard = []
         self.vp = 0
+
+        #add cards to deck and shuffle the deck
+        for i in range(0, 7):
+            self.deck.append(Card(1))
+        for i in range(0, 3):
+            self.deck.append(Card(4))
+        random.shuffle(self.deck)
+        
+        
     
     #do player discard methods go here??
     #TODO: determine whether Player classes should keep track of shuffles or the main loop should keep track of shuffles per turn
+
+    #code for drawing cards
+    #TODO: write code for case where deck has less than 5 cards
+    def draw(self):
+        for i in range(0, 5):
+            self.hand.append(self.deck[0])
+            self.deck.pop(0)      
+
+debug = Player()
+debug.draw()
+for card in debug.deck:
+    print(card)
